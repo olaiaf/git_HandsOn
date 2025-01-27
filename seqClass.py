d11a10259fd91e099c94b02d7c30e7b553ca8ea7
Olaia <you@example.com>
2025-01-27 18:31:17 +0100

    FOUND IT message

--- seqClass.py
+++ seqClass.py
@@ -27,7 +27,7 @@ if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
-        print("FOUND")
+        print("FOUND IT")
     else:
         print("DIDT FOUND IT")
 parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
