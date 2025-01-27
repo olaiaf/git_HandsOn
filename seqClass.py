27798a16c72072f45e168b8194552cf89c10abc0
Olaia <you@example.com>
2025-01-27 17:45:32 +0100

    find simple motifs new function

--- seqClass.py
+++ seqClass.py
@@ -23,3 +23,11 @@ if re.search('^[ACGTU]+$', args.seq):
         print ('The sequence can be DNA or RNA')
 else:
     print ('The sequence is not DNA nor RNA')
+if args.motif:
+    args.motif = args.motif.upper()
+    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
+    if re.search(args.motif, args.seq):
+        print("FOUND")
+    else:
+        print("NOT FOUND")
+parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
