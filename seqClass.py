205289fdfc8124f252883ee30bd24df07afe0b0b
Olaia <you@example.com>
2025-01-30 13:05:38 +0100

    Fix the seqClass.py script so that it is able to classify correctly any RNA or DNA sequence

--- seqClass.py
+++ seqClass.py
@@ -2,14 +2,14 @@
 #!/usr/bin/env python
 
 # MY SCRIPT
-import sys, re
+
+import sys
+import re
 from argparse import ArgumentParser
 
- 
-
-
-parser = ArgumentParser(description = '1 a sequence as DNA or RNA')
-parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+parser = ArgumentParser(description="Classify a sequence as DNA or RNA and optionally search for a motif.")
+parser.add_argument("-s", "--seq", type=str, required=True, help="Input sequence")
+parser.add_argument("-m", "--motif", type=str, required=False, help="Motif to search for in the sequence")
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -17,21 +17,28 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-args.seq = args.seq.upper()                 # Note we just added this line
-if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
-        print ('The sequence is DNA')
-    elif re.search('U', args.seq):
-        print ('The sequence is RNA')
+# Convert sequence to uppercase
+args.seq = args.seq.upper()
+
+# Validate sequence
+if re.fullmatch(r'^[ACGTU]+$', args.seq):
+    if 'T' in args.seq and 'U' in args.seq:
+        print("The sequence contains both T and U, which is invalid.")
+    elif 'T' in args.seq:
+        print("The sequence is DNA")
+    elif 'U' in args.seq:
+        print("The sequence is RNA")
     else:
-        print ('The sequence can be DNA or RNA')
+        print("The sequence can be DNA or RNA")
 else:
-    print ('The sequence is not DNA nor RNA')
+    print("The sequence is not DNA nor RNA")
+
+# Motif search (if provided)
 if args.motif:
     args.motif = args.motif.upper()
-    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
+    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end="")
     if re.search(args.motif, args.seq):
         print("FOUND IT")
     else:
-        print("DIDT FOUND IT")
-parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
+        print("DIDN'T FIND IT")
+
